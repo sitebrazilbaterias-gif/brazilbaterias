9b4bef7e38c10073439623c28f45cc0a598f4517
sitebrazilbaterias-gif <sitebrazilbaterias@gmail.com>
2025-10-17 03:42:28 +0000

    Ajustes de rolagem da pagina e texto para versão mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
 
   return (
     <header
-      className={`ffixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
+      className={`relative w-full transition-all duration-300 ${
         isScrolled ? "bg-[#284835]/95 backdrop-blur-md shadow-md text-white" : "bg-background/80 backdrop-blur-sm"
       }`}
     >
